c9862769d0ca01eb098086a9bf502c64159e35f7
sudo-Vanila <deepbasak100@gmail.com>
2025-04-22 20:17:59 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,103 @@
-# 3d-portfolio
+<div align="center">
+  <br />
+    <a href="" target="_blank">
+      <img src="public/images/readme.png" alt="Project Banner">
+    </a>
+  <br />
+
+  <div>
+    <img src="https://img.shields.io/badge/-Three.js-black?style=for-the-badge&logo=three.js&logoColor=white" />
+    <img src="https://img.shields.io/badge/-GSAP-88CE02?style=for-the-badge&logo=greensock&logoColor=white" />
+    <img src="https://img.shields.io/badge/-Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white" />
+  </div>
+
+  <h3 align="center">Interactive 3D Portfolio Website</h3>
+
+   <div align="center">
+     Build this project step by step with HTML + CSS + JAVASCRIPT + REACT.JS + NODE.JS
+    </div>
+</div>
+
+
+## <a name="introduction">🤖 Introduction</a>
+
+The 3D Portfolio project is a highly engaging personal website that features animated 3D scenes, smooth camera transitions, interactive model showcases, and responsive design. It’s ideal for developers, designers, or freelancers looking to stand out in the digital crowd.
+
+## <a name="tech-stack">⚙️ Tech Stack</a>
+
+- Three.js
+- React Three Fiber
+- Drei
+- GSAP
+- Tailwind CSS
+- Vite
+- React 19
+
+## <a name="features">🔋 Features</a>
+
+### Features of the 3D Portfolio Project
+
+👉 Animated 3D models and reveal animations
+
+👉 Realistic lighting and shadows
+
+👉 GSAP-powered scroll interactions
+
+👉 Responsive design with Tailwind CSS and Flexbox/Grid
+
+👉 Micro Interactions
+
+👉 Multi-section layout (About, Projects, Contact)
+
+👉 Mobile optimized 3D experience
+
+and many more, including code architecture and reusability.
+
+## <a name="quick-start">🤸 Quick Start</a>
+
+Follow these steps to set up the project locally on your machine.
+
+**Prerequisites**
+
+Make sure you have the following installed on your machine:
+
+- [Git](https://git-scm.com/)
+- [Node.js](https://nodejs.org/en)
+- [npm](https://www.npmjs.com/) (Node Package Manager)
+
+**Cloning the Repository**
+
+```bash
+git clone https://github.com/adrianhajdin/3d-portfolio.git
+cd 3d-portfolio
+```
+
+**Installation**
+
+Install the project dependencies using npm:
+
+```bash
+npm install
+```
+
+**Set Up Environment Variables**
+
+Create a new file named `.env` in the root of your project and add the following content:
+
+```env
+VITE_APP_EMAILJS_SERVICE_ID=
+VITE_APP_EMAILJS_TEMPLATE_ID=
+VITE_APP_EMAILJS_PUBLIC_KEY=
+```
+
+Replace the placeholder values with your actual **[EmailJS](https://www.emailjs.com/)** credentials.
+
+**Running the Project**
+
+```bash
+npm run dev
+```
+
+Open [http://localhost:5173](http://localhost:5173/) in your browser to view the project.
+
+
---
+++ src/constants/index.js
@@ -0,0 +1,273 @@
+const navLinks = [
+  {
+    name: "Work",
+    link: "#work",
+  },
+  {
+    name: "Experience",
+    link: "#experience",
+  },
+  {
+    name: "Skills",
+    link: "#skills",
+  },
+  {
+    name: "Testimonials",
+    link: "#testimonials",
+  },
+];
+
+const words = [
+  { text: "Ideas", imgPath: "/images/ideas.svg" },
+  { text: "Concepts", imgPath: "/images/concepts.svg" },
+  { text: "Designs", imgPath: "/images/designs.svg" },
+  { text: "Code", imgPath: "/images/code.svg" },
+  { text: "Ideas", imgPath: "/images/ideas.svg" },
+  { text: "Concepts", imgPath: "/images/concepts.svg" },
+  { text: "Designs", imgPath: "/images/designs.svg" },
+  { text: "Code", imgPath: "/images/code.svg" },
+];
+
+const counterItems = [
+  { value: 3, suffix: "+", label: "Years of Experience" },
+  { value: 5, suffix: "+", label: "Collaboration" },
+  { value: 12, suffix: "+", label: "Completed Projects" },
+  { value: 95, suffix: "%", label: "Retention Rate" },
+];
+
+const logoIconsList = [
+  {
+    imgPath: "/images/logos/company-logo-1.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-2.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-3.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-4.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-5.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-6.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-7.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-8.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-9.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-10.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-11.png",
+  },
+];
+
+const abilities = [
+  {
+    imgPath: "/images/seo.png",
+    title: "Quality-Driven Development",
+    desc: "Crafting high-quality solutions with meticulous attention to detail in every line of code.",
+  },
+  {
+    imgPath: "/images/chat.png",
+    title: "Clear & Consistent Communication",
+    desc: "Ensuring transparency and regular updates throughout the project lifecycle.",
+  },
+  {
+    imgPath: "/images/time.png",
+    title: "On-Time Delivery",
+    desc: "Respecting deadlines while maintaining top-notch quality and precision.",
+  },
+];
+
+const techStackImgs = [
+  {
+    name: "React Developer",
+    imgPath: "/images/logos/react.png",
+  },
+  {
+    name: "Java Developer",
+    imgPath: "/images/logos/python.svg",
+  },
+  {
+    name: "Backend Developer",
+    imgPath: "/images/logos/node.png",
+  },
+  {
+    name: "Interactive Developer",
+    imgPath: "/images/logos/three.png",
+  },
+  {
+    name: "Project Manager",
+    imgPath: "/images/logos/git.svg",
+  },
+];
+
+const techStackIcons = [
+  {
+    name: "React Developer",
+    modelPath: "/models/react_logo-transformed.glb",
+    scale: 1,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "Python Developer",
+    modelPath: "/models/python-transformed.glb",
+    scale: 0.8,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "Backend Developer",
+    modelPath: "/models/node-transformed.glb",
+    scale: 5,
+    rotation: [0, -Math.PI / 2, 0],
+  },
+  {
+    name: "Interactive Developer",
+    modelPath: "/models/three.js-transformed.glb",
+    scale: 0.05,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "Project Manager",
+    modelPath: "/models/git-svg-transformed.glb",
+    scale: 0.05,
+    rotation: [0, -Math.PI / 4, 0],
+  },
+];
+
+const expCards = [
+  {
+    review: "Deep brought creativity and technical expertise to the team, significantly improving our frontend performance. His work has been invaluable in delivering faster experiences.",
+    imgPath: "/images/exp1.png",
+    logoPath: "/images/logo1.png",
+    title: "Frontend Developer",
+    date: "November 2024 - March 2025",
+    responsibilities: [
+      "Developed and maintained user-facing features for the Hostinger website.",
+      "Collaborated closely with UI/UX designers to ensure seamless user experiences.",
+      "Optimized web applications for maximum speed and scalability.",
+    ],
+  },
+  {
+    review: "Deep’s contributions to Docker's web applications have been outstanding. He approaches challenges with a problem-solving mindset.",
+    imgPath: "/images/exp2.png",
+    logoPath: "/images/logo2.png",
+    title: "Full Stack Developer",
+    date: "March 2025 - Present",
+    responsibilities: [
+      "Led the development of Docker's web applications, focusing on scalability.",
+      "Worked with backend engineers to integrate APIs seamlessly with the frontend.",
+      "Contributed to open-source projects that were used with the Docker ecosystem.",
+    ],
+  },
+  {
+    review: "Deep’s work on SharEX Exam Website brought a high level of quality and efficiency. He delivered solutions that enhanced our Website experience & meet our product goals.",
+    imgPath: "/images/exp3.png",
+    logoPath: "/images/logo3.png",
+    title: "React Native Developer",
+    date: "March 2025 - Present",
+    responsibilities: [
+      "Built cross-platform Exam Website using React Native, integrating with SharEx backend services.",
+      "Improved app performance and user experience through code optimization and testing.",
+      "Coordinated with the product team to implement features based on feedback.",
+    ],
+  },
+];
+
+const expLogos = [
+  {
+    name: "logo1",
+    imgPath: "/images/logo1.png",
+  },
+  {
+    name: "logo2",
+    imgPath: "/images/logo2.png",
+  },
+  {
+    name: "logo3",
+    imgPath: "/images/logo3.png",
+  },
+];
+
+const testimonials = [
+  {
+    name: "Devesh Kumar",
+    mentions: "@deveshkumar",
+    review:
+      "I can’t say enough good things about Deep. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.",
+    imgPath: "/images/client1.png",
+  },
+  {
+    name: "Amardeep Kumar",
+    mentions: "@amardeepkumar",
+    review:
+      "Working with Deep was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.",
+    imgPath: "/images/client3.png",
+  },
+  {
+    name: "Abhishek Ranjan",
+    mentions: "@abhishekranjan",
+    review:
+      "Collaborating with Deep was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Deep is the ideal partner.",
+    imgPath: "/images/client2.png",
+  },
+  {
+    name: "Mohit Kumar",
+    mentions: "@mohitkumar",
+    review:
+      "Deep was a pleasure to work with. He turned our outdated website into a fresh, intuitive platform that’s both modern and easy to navigate. Fantastic work overall.",
+    imgPath: "/images/client5.png",
+  },
+  {
+    name: "Ayush",
+    mentions: "@ayush",
+    review:
+      "Deep’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional!",
+    imgPath: "/images/client4.png",
+  },
+];
+
+const socialImgs = [
+  {
+    name: "insta",
+    imgPath: "/images/insta.png",
+  },
+  {
+    name: "fb",
+    imgPath: "/images/fb.png",
+  },
+  {
+    name: "x",
+    imgPath: "/images/x.png",
+    link: "https://x.com/deep766411",
+  },
+  {
+    name: "linkedin",
+    imgPath: "/images/linkedin.png",
+    link: "https://www.linkedin.com/in/sudodeep15/",
+  },
+];
+
+export {
+  words,
+  abilities,
+  logoIconsList,
+  counterItems,
+  expCards,
+  expLogos,
+  testimonials,
+  socialImgs,
+  techStackIcons,
+  techStackImgs,
+  navLinks,
+};
